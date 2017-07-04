0f8cfff06fac779e052a8021e85a019de4919821
Dominika Sendor <dominika.sendor@miquido.com>
2017-07-04 14:00:42 +0200

    [Profile] Key Data test

---
+++ src/com/company/ProfileScreenTest.java
@@ -0,0 +1,18 @@
+package com.company;/**
+ * Created by dominika on 04.07.17.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ProfileScreenTest extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
